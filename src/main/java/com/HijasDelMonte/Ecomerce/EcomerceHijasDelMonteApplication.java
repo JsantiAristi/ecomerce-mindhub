39acc45587845dfdd16284a76c34f2af080ed13e
Santi <jsanti.restrepo.2021@gmail.com>
2023-05-24 09:29:46 -0500

    Inicio el proyecto

---
+++ src/main/java/com/HijasDelMonte/Ecomerce/EcomerceHijasDelMonteApplication.java
@@ -0,0 +1,13 @@
+package com.HijasDelMonte.Ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceHijasDelMonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceHijasDelMonteApplication.class, args);
+	}
+
+}
