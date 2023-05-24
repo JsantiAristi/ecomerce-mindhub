3782adb31a17fd1a3c15cd518205bbf787b37f8a
Santi <jsanti.restrepo.2021@gmail.com>
2023-05-24 11:06:39 -0500

    Agrego archivos importantes

---
+++ src/main/java/com/HijasDelMonte/Ecomerce/Utilidades/ClientesUtilidades.java
@@ -0,0 +1,4 @@
+package com.HijasDelMonte.Ecomerce.Utilidades;
+
+public class ClientesUtilidades {
+}
---
+++ src/main/resources/static/web/js/index.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return {
+    // Inicializamos las variables
+        }
+    },
+}).mount("#app")
