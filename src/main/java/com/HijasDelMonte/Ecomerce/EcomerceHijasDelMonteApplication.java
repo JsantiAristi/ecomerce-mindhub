e53fe7fa00d6f0fa2855f5d77203aeb30a1e00e7
ceugeniag <gonzalezceugenia@gmail.com>
2023-05-30 16:39:59 -0300

    cambios en el footer

--- src/main/java/com/HijasDelMonte/Ecomerce/EcomerceHijasDelMonteApplication.java
+++ src/main/java/com/HijasDelMonte/Ecomerce/EcomerceHijasDelMonteApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+
 import java.time.LocalDate;
 
 @SpringBootApplication
